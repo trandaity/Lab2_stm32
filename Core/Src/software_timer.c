8c753c43d4bd1b87f96f4b5ff34096b1f50545d4
Ty Tran Dai <trandaity151@gmail.com>
2022-11-18 23:00:41 +0700

    Implement the updateClockBuffer()

--- Core/Src/software_timer.c
+++ Core/Src/software_timer.c
@@ -11,6 +11,8 @@ int timer1_counter = 0;
 int timer1_flag = 0;
 int timer2_counter = 0;
 int timer2_flag = 0;
+int timer3_counter = 0;
+int timer3_flag = 0;
 
 void setTimer1(int duration){
 	timer1_counter = duration;
@@ -21,6 +23,11 @@ void setTimer2(int duration){
 	timer2_counter = duration;
 	timer2_flag = 0;
 }
+
+//void setTimer3(int duration) {
+//	timer3_counter = duration;
+//	timer3_flag = 0;
+//}
 
 void timerRun(){
 	if(timer1_counter > 0){
@@ -38,4 +45,13 @@ void timerRun(){
 			timer2_flag = 1;
 		}
 	}
+
+//	if(timer3_counter > 0)
+//	{
+//		timer3_counter--;
+//		if(timer3_counter == 0)
+//		{
+//			timer3_flag = 1;
+//		}
+//	}
 }
--- Core/Src/updateClockBuffer.c
+++ Core/Src/updateClockBuffer.c
@@ -9,5 +9,8 @@
 #include "global.h"
 
 void updateClockBuffer() {
-
+	led_buffer[3] = minute%10;
+	led_buffer[2] = minute/10;
+	led_buffer[1] = hour%10;
+	led_buffer[0] = hour/10;
 }
